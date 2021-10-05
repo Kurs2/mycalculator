da34eeb020faeb8424c6361dd48c37980b69e6b6
Kurs2 <cgs.kurs002@gmail.com>
2021-10-05 09:30:35 +0200

    created MyCalculator class

---
+++ src/main/java/com/ats/mycalculator/MyCalculator.java
@@ -0,0 +1,39 @@
+package com.ats.mycalculator;
+
+
+import java.util.Scanner;
+
+public class MyCalculator {
+    public static void main(String[] args) {
+        System.out.println("Was wollen sie machen +,-,*");
+        Scanner sc = new Scanner(System.in);
+
+        String eingabe = sc.nextLine();
+
+        sc.close();
+
+        MyCalculator calc = new MyCalculator();
+
+
+        switch (eingabe){
+            case "+": calc.add();break;
+            case "-": calc.sub();break;
+            case "*": calc.prod();break;
+            default :
+                System.out.println("Error Error");System.exit(0);
+        }
+        
+    }
+
+    private void prod(){
+
+    }
+
+    private void sub(){
+
+    }
+
+    private void add(){
+
+    }
+}
